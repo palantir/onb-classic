f51ccbc9e29e3e9f6193f75bf851a388bcbad7d4
Dan Berkowitz <dberkowitz@palantir.com>
2026-01-28 10:25:12 -0500

    Update to handle different version numbers

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -20,5 +20,5 @@ if [ "$TYPE" == "release" ]; then
 fi
 
 URL="https://artifactory.PUBLIC_URL_NEEDED/artifactory/internal-jar-${TYPE}/com/palantir/onb-classic/onb-classic/${SNAPSHOT_VERSION}/onb-classic-${SNAPSHOT_VERSION}.jar"
-echo Publishing to ${URL} from ${CACHE_DIR}/onb-classic.jar
-curl --silent --show-error --fail --user "${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD}" --request PUT ${URL} --upload-file ${CACHE_DIR}/onb-classic.jar
+echo Publishing to ${URL} from ${CACHE_DIR}/onb-classic-all.jar
+curl --silent --show-error --fail --user "${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD}" --request PUT ${URL} --upload-file ${CACHE_DIR}/onb-classic-all.jar
